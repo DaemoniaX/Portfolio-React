cccc98b917936b9a9eb01b2deda270710de39f5a
DaemoniaX <antonin.marolleau@gmail.com>
2026-01-31 17:00:10 +0100

    browserRouter->Hashrooter pour selenium tests

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,21 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './css/index.css';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';/*on passe de browserrouter a hashrouter a cause de githubpages pour les tests selenium*/
 import { FavoritesProvider } from './contexts/FavoritesContext.jsx';
 
 createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
+      <FavoritesProvider>
+        <App />
+      </FavoritesProvider>
+    </HashRouter>
+  </StrictMode>
+);
+
+
+/*createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <FavoritesProvider>
@@ -13,4 +24,4 @@ createRoot(document.getElementById('root')).render(
       </FavoritesProvider>
     </BrowserRouter>
   </StrictMode>
-);
+);*/
